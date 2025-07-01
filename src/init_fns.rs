d895519db1d2816808bddfb16d60c0298a674389
lmr97 <lmreid1997@gmail.com>
2025-07-01 17:36:17 -0600

    case-insensitive search for "touchpad", better error messages

--- src/init_fns.rs
+++ src/init_fns.rs
@@ -6,9 +6,11 @@ pub mod config {
     use std::path::PathBuf;
 
     #[derive(Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
     pub struct Configuration {
         pub acceleration: f64,
         pub drag_end_delay: u64, // in milliseconds
+        pub min_motion: f64,
     }
 
     impl Default for Configuration {
@@ -16,6 +18,7 @@ pub mod config {
             Configuration {
                 acceleration: 1.0,
                 drag_end_delay: 0,
+                min_motion: 0.2,
             }
         }
     }
@@ -90,16 +93,20 @@ pub mod libinput_init {
         all_inputs.udev_assign_seat("seat0").unwrap();
 
 
+        // Libinput adds "touchpad" to the device you use for a trackpad.
+        // This finds theat device among all active ones on your computer.
         let trackpad_find_opt = all_inputs.find(
-            |event| event.device().name().contains("Touchpad")
+            |event| event.device().name().to_lowercase().contains("touchpad")
         );
         
         let udev_name = match trackpad_find_opt {
 
             Some(tp_add_ev) => tp_add_ev.device().sysname().to_string(),
             None => panic!("ERROR: there does not seem to be a trackpad on your \
-                device (a device that has both 'pointer' and 'gesture' \
-                capabilities as defined by libinput).")
+                device (a device with 'touchpad' in its libinput name). If you're \
+                seeing this, please submit a Github issue at \
+                https://github.com/lmr97/linux-3-finger-drag/issues and I will look \
+                into it as soon as possible.")
         };
 
         let mut real_trackpad = Libinput::new_from_path(Interface);
@@ -116,10 +123,15 @@ pub mod libinput_init {
             },
             None => Err(
                 Error::new(
-                    ErrorKind::PermissionDenied, 
-                    "\nERROR: Could not load any touchpad device. \
-                    If you know it exists at the given path, you probably \
-                    need root permissions to access it.\n"
+                    ErrorKind::NotFound, 
+                    format!("\nERROR: Could not load the touchpad device \
+                    named `/dev/input/{udev_name}`. It may also be a permissions \
+                    error, but the underlying crate (input.rs) does not raise \
+                    errors when a device cannot be loaded, so it's unclear. \
+                    Please submit a Github issue at https://github.com/lmr97/linux-3-finger-drag/issues \
+                    whether you sort this out or not, so as to help others in the \
+                    same situation, and help me develop a better program. Thank \
+                    you for trying it out!")
                 )
             )
         }
