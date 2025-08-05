4686397273b93cab69b39a5fd3bb1c908862bd56
lmr97 <lmreid1997@gmail.com>
2025-08-04 22:15:10 -0600

    adding benchmark

--- src/benches/cloning.rs
+++ src/benches/cloning.rs
@@ -5,16 +5,21 @@
 // max for the configuration to be cloned).
 
 use std::hint::black_box;
+use smol::channel::unbounded;
 use criterion::{criterion_group, criterion_main, Criterion};
 use linux_3_finger_drag::{
     init::config::Configuration,
-    runtime::virtual_trackpad
+    runtime::{
+        event_handler::CancelMouseUpDelay,
+        virtual_trackpad
+    }
 };
 
 pub fn clone_virtual_trackpad(c: &mut Criterion) {
-    let vtp = black_box(virtual_trackpad::start_handler().unwrap());
+    let (_, rx) = unbounded::<CancelMouseUpDelay>();
+    let vtp = black_box(virtual_trackpad::start_handler(rx).unwrap());
     
-    c.bench_function("vtrack cloning", |b| {
+    c.bench_function("VirtualTrackpad cloning", |b| {
         b.iter(|| {
             let vtp2 = vtp.clone();
             black_box(vtp2);
@@ -25,7 +30,7 @@ pub fn clone_virtual_trackpad(c: &mut Criterion) {
 fn clone_config(c: &mut Criterion) {
     let cfg = Configuration::default();
     
-    c.bench_function("config cloning", |b| {
+    c.bench_function("Configuration cloning", |b| {
         b.iter(|| {
             let cfg2 = cfg.clone();
             std::hint::black_box(cfg2);
@@ -33,5 +38,5 @@ fn clone_config(c: &mut Criterion) {
     });
 }
 
-criterion_group!(clones, clone_config, clone_virtual_trackpad);
+criterion_group!(clones, clone_virtual_trackpad, clone_config);
 criterion_main!(clones);
